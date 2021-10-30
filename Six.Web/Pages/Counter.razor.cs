ae176e957e1c2cff006d5db535297b81908cacbc
Knut Jelitto <knut.jelitto@live.com>
2021-10-30 04:00:27 +0200

    changes

---
+++ Six.Web/Pages/Counter.razor.cs
@@ -0,0 +1,11 @@
+namespace Six.Web.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
